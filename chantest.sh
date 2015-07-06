ec431649525e3941150bd4adfd6d67e2eacf4c11
constantine <constantine@const>
2015-07-06 20:22:26 +0600

    Доделать вызов скрипта с параметрами

--- chantest.sh
+++ chantest.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3.4
-import os
+#import os
 import sys
-import time
+#import time
 
-def testchan():
-	host = "ya.ru"
-	res = os.system("ping -c 1 " + host)
-	if res == 0:
-		print (host, ' is up')
-		os.system("cvlc " + '/home/constantine/Starcraft\ II\ Terran\ Unit\ Sounds\ -\ Soundboard.com\ -\ Free\ Sou-1.mpeg ' + '--play-and-exit')
-		sys.exit()
-	else:
-		print (host, ' is down')
-		return True
+print (sys.argv[0])
+print (sys.argv[1])
 
-if __name__ == '__main__':
-	while True:
-		testchan()
-		time.sleep(20)
+#def testchan():
+	#host = "ya.ru"
+	#res = os.system("ping -c 1 " + host)
+	#if res == 0:
+		#print (host, ' is up')
+		#os.system("cvlc " + '/home/constantine/Starcraft\ II\ Terran\ Unit\ Sounds\ -\ Soundboard.com\ -\ Free\ Sou-1.mpeg ' + '--play-and-exit')
+		#sys.exit()
+	#else:
+		#print (host, ' is down')
+		#return True
+
+#if __name__ == '__main__':
+	#while True:
+		#testchan()
+		#time.sleep(20)
