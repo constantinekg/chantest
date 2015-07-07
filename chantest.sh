82463c094ba343d81c660b88abefab1772028e41
constantine <constantine@const>
2015-07-07 14:30:02 +0600

    Добавлена функция вывода звука через системный динамик (музыка из звёздных войн)

--- chantest.sh
+++ chantest.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3.4
-#import os
+import os
 import sys
-#import time
+import time
 
-print (sys.argv[0])
-print (sys.argv[1])
 
-#def testchan():
-	#host = "ya.ru"
-	#res = os.system("ping -c 1 " + host)
-	#if res == 0:
-		#print (host, ' is up')
+def testchan():
+	host = "city.kg"
+	res = os.system("ping -c 1 " + host)
+	if res == 0:
+		print (host, ' is up')
 		#os.system("cvlc " + '/home/constantine/Starcraft\ II\ Terran\ Unit\ Sounds\ -\ Soundboard.com\ -\ Free\ Sou-1.mpeg ' + '--play-and-exit')
-		#sys.exit()
-	#else:
-		#print (host, ' is down')
-		#return True
+		os.system("beep " + '-l 350 -f 392 -D 100 -n -l 350 -f 392 -D 100 -n -l 350 -f 392 -D 100 -n -l 250 -f 311.1 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 350 -f 392 -D 100 -n -l 250 -f 311.1 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 700 -f 392 -D 100 -n -l 350 -f 587.32 -D 100 -n -l 350 -f 587.32 -D 100 -n -l 350 -f 587.32 -D 100 -n -l 250 -f 622.26 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 350 -f 369.99 -D 100 -n -l 250 -f 311.1 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 700 -f 392 -D 100 -n -l 350 -f 784 -D 100 -n -l 250 -f 392 -D 100 -n -l 25 -f 392 -D 100 -n -l 350 -f 784 -D 100 -n -l 250 -f 739.98 -D 100 -n -l 25 -f 698.46 -D 100 -n -l 25 -f 659.26 -D 100 -n -l 25 -f 622.26 -D 100 -n -l 50 -f 659.26 -D 400 -n -l 25 -f 415.3 -D 200 -n -l 350 -f 554.36 -D 100 -n -l 250 -f 523.25 -D 100 -n -l 25 -f 493.88 -D 100 -n -l 25 -f 466.16 -D 100 -n -l 25 -f 440 -D 100 -n -l 50 -f 466.16 -D 400 -n -l 25 -f 311.13 -D 200 -n -l 350 -f 369.99 -D 100 -n -l 250 -f 311.13 -D 100 -n -l 25 -f 392 -D 100 -n -l 350 -f 466.16 -D 100 -n -l 250 -f 392 -D 100 -n -l 25 -f 466.16 -D 100 -n -l 700 -f 587.32 -D 100 -n -l 350 -f 784 -D 100 -n -l 250 -f 392 -D 100 -n -l 25 -f 392 -D 100 -n -l 350 -f 784 -D 100 -n -l 250 -f 739.98 -D 100 -n -l 25 -f 698.46 -D 100 -n -l 25 -f 659.26 -D 100 -n -l 25 -f 622.26 -D 100 -n -l 50 -f 659.26 -D 400 -n -l 25 -f 415.3 -D 200 -n -l 350 -f 554.36 -D 100 -n -l 250 -f 523.25 -D 100 -n -l 25 -f 493.88 -D 100 -n -l 25 -f 466.16 -D 100 -n -l 25 -f 440 -D 100 -n -l 50 -f 466.16 -D 400 -n -l 25 -f 311.13 -D 200 -n -l 350 -f 392 -D 100 -n -l 250 -f 311.13 -D 100 -n -l 25 -f 466.16 -D 100 -n -l 300 -f 392.00 -D 150 -n -l 250 -f 311.13 -D 100 -n -l 25 -f 466.16 -D 100 -n -l 700 -f 392')
+		sys.exit()
+	else:
+		print (host, ' is down')
+		return True
 
-#if __name__ == '__main__':
-	#while True:
-		#testchan()
-		#time.sleep(20)
+if __name__ == '__main__':
+	while True:
+		testchan()
+		time.sleep(10)
--- test.py
+++ test.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3.4
-import getopt, sys
 
-def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print(err) # will print something like "option -a not recognized"
+import sys
+import getopt
+
+def usage():
+	print ('How to use...')
+
+try:
+    opts, args = getopt.getopt(argv, 'm:p:h', ['miner=', 'params=', 'help'])
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        usage()
         sys.exit(2)
-    output = None
-    verbose = False
-    for o, a in opts:
-        if o == "-v":
-            verbose = True
-        elif o in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif o in ("-o", "--output"):
-            output = a
-        else:
-            assert False, "unhandled option"
-    # ...
+    elif opt in ('-m', '--miner'):
+        miner_name = arg
+    elif opt in ('-p', '--params'):
+        params = arg
+    else:
+        usage()
+        sys.exit(2)
 
 if __name__ == "__main__":
     main()
